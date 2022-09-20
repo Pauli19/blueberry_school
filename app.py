8cd3cd657e9f8bfe212bbc21df3559541f1f9e8b
Dora <110313250+Pauli19@users.noreply.github.com>
2022-09-20 17:28:51 -0500

    Create 404 error page (#10)
    * Create 404 error page
    
    * Update app.py
    
    Co-authored-by: M <lmiguelvargasf@gmail.com>
    
    * Update templates/404.html.jinja
    
    Co-authored-by: M <lmiguelvargasf@gmail.com>
    
    * Update app.py
    
    * Update title in 404 page
    
    * Update spaces
    
    Co-authored-by: M <lmiguelvargasf@gmail.com>

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template('index.html.jinja')
+
+
+@app.errorhandler(404)
+def page_not_found(exc):
+    return render_template('404.html.jinja'), 404
