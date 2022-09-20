9ef93779b4fadafff0dba29114321ef5ffa0c01a
Dora Gaibor <dpgaibor@outlook.com>
2022-09-20 15:42:48 -0500

    Create 404 error page

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template('index.html.jinja')
+
+
+@app.errorhandler(404)
+def page_not_found(exc):
+	print(exc)
+	print(type(exc))
+	return render_template('404.html.jinja'), 400
