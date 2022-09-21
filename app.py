5a9e2def56372447e4ad50f20fde91cd4f18d5bc
Dora Gaibor <dpgaibor@outlook.com>
2022-09-20 19:10:09 -0500

    Create custom 500 page

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ def index():
 @app.errorhandler(404)
 def page_not_found(exc):
     return render_template('404.html.jinja'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(exc):
+    return render_template('500.html.jinja'), 500
