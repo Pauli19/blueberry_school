a16007b48aee21c86a9a7f872ab6c5b71dbdd130
Dora Gaibor <dpgaibor@outlook.com>
2022-09-21 20:13:51 -0500

    Initialize Bootstrap5

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from ensurepip import bootstrap
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap5(app)
 
 @app.get("/")
 def index():
