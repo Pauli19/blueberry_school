1ab71af743412872c339228ecb9ee96f5eaa553d
Mïk <m@mathsistor.com>
2022-09-25 01:27:33 -0500

    Fix pre-commit warnings on app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""This module contains creation and configuration of the Flask app."""
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
@@ -9,14 +11,17 @@ bootstrap = Bootstrap5(app)
 
 @app.get("/")
 def index():
+    """View function for "/" route."""
     return render_template("index.html.jinja")
 
 
 @app.errorhandler(404)
-def page_not_found(exc):
+def page_not_found(exc):  # pylint: disable=unused-argument
+    """Error handler for 404 status code."""
     return render_template("404.html.jinja"), 404
 
 
 @app.errorhandler(500)
-def internal_server_error(exc):
+def internal_server_error(exc):  # pylint: disable=unused-argument
+    """Error handler for 500 status code."""
     return render_template("500.html.jinja"), 500
