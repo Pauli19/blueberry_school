9ff568b7f78f1ec5dad4696f127028450fb515d1
M <m@mathsistor.com>
2022-09-26 15:20:50 -0500

    Add spacing

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 bootstrap = Bootstrap5(app)
 
 
