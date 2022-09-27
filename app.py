f089581ea51311846eb799c136ed05e367245a35
Dora <110313250+Pauli19@users.noreply.github.com>
2022-09-26 21:35:54 -0500

    Add type hints in app.py (#22)

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import InternalServerError, NotFound
 
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
@@ -19,18 +20,20 @@ bootstrap = Bootstrap5(app)
 
 
 @app.get("/")
-def index():
+def index() -> str:
     """View function for "/" route."""
     return render_template("index.html.jinja")
 
 
 @app.errorhandler(404)
-def page_not_found(exc):  # pylint: disable=unused-argument
+def page_not_found(exc: NotFound) -> tuple[str, int]:  # pylint: disable=unused-argument
     """Error handler for 404 status code."""
     return render_template("404.html.jinja"), 404
 
 
 @app.errorhandler(500)
-def internal_server_error(exc):  # pylint: disable=unused-argument
+def internal_server_error(
+    exc: InternalServerError,  # pylint: disable=unused-argument
+) -> tuple[str, int]:
     """Error handler for 500 status code."""
     return render_template("500.html.jinja"), 500
