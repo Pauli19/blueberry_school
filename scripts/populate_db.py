b2715c8a0701fcc896d6ad917027e228850940d9
Dora Gaibor <dpgaibor@outlook.com>
2022-09-27 20:32:19 -0500

    Create script to populate database

---
+++ scripts/populate_db.py
@@ -0,0 +1,25 @@
+"""
+This file contains a script to populate the database.
+"""
+
+from app import User, db
+
+users = [
+    User(
+        first_name="Luis",
+        second_name="Miguel",
+        first_surname="Vargas",
+        second_surname="Fonseca",
+        email="lmiguelvargasf@gmail.com",
+    ),
+    User(
+        first_name="Dora",
+        second_name="Paulina",
+        first_surname="Gaibor",
+        second_surname="Llanos",
+        email="dpgaibor@outlook.com",
+    ),
+]
+session = db.session
+session.add_all(users)  # pylint: disable=no-member
+session.commit()  # pylint: disable=no-member
