591d49385bfc5780f6d18f2615aa65211da6894d
Mïk <m@mathsistor.com>
2022-09-29 12:49:13 -0500

    Update script to populate db

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -2,24 +2,12 @@
 This file contains a script to populate the database.
 """
 
-from app.models import User, db
+import os
 
-users = [
-    User(
-        first_name="Luis",
-        second_name="Miguel",
-        first_surname="Vargas",
-        second_surname="Fonseca",
-        email="lmiguelvargasf@gmail.com",
-    ),
-    User(
-        first_name="Dora",
-        second_name="Paulina",
-        first_surname="Gaibor",
-        second_surname="Llanos",
-        email="dpgaibor@outlook.com",
-    ),
-]
-session = db.session
-session.add_all(users)  # pylint: disable=no-member
-session.commit()  # pylint: disable=no-member
+from factories import UserFactory
+
+first_name = os.environ["FIRST_NAME"]
+first_surname = os.environ["FIRST_SURNAME"]
+UserFactory(
+    first_name=first_name, first_surname=first_surname, email="admin@example.com"
+)
