9dc3729b9ff9d469a909e38fdd26e361245061b8
Mïk <m@mathsistor.com>
2022-09-29 11:01:03 -0500

    Add TODOs

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@
 This package contains the implementation of a factory
 function to create and configure a Flask app.
 """
+
 from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+
+# pylint: disable=fixme
 
 bootstrap = Bootstrap5()
 db = SQLAlchemy()
@@ -19,10 +22,12 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # TODO: improve extension initialization
     bootstrap.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # TODO: improve blueprint registration
     from .main import main as main_blueprint  # pylint: disable=import-outside-toplevel
 
     app.register_blueprint(main_blueprint)
