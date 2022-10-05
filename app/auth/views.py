a36d2c82fa012ad13c25edfe7fb3ec92fb57580f
Dora Gaibor <dpgaibor@outlook.com>
2022-10-05 16:35:36 -0500

    Implement logout view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@
 This module contains view functions associated to `auth` blueprint.
 """
 
-from flask import render_template
+from flask import Response, flash, redirect, render_template, url_for
+from flask_login import login_required, logout_user
 
 from . import auth
 from .forms import LoginForm
@@ -13,3 +14,12 @@ def login_get() -> str:
     """View function for "/login" route."""
     form = LoginForm()
     return render_template("auth/login.html.jinja", form=form)
+
+
+@auth.get("/logout")
+@login_required
+def logout() -> Response:
+    """View function for "logout" route."""
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for("main.index"))
