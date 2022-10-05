96e8c154a8edee59c744ae5f5422b67eea2293cd
Dora Gaibor <dpgaibor@outlook.com>
2022-10-05 15:19:21 -0500

    Pass form to template

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,9 +5,11 @@ This module contains view functions associated to `auth` blueprint.
 from flask import render_template
 
 from . import auth
+from .forms import LoginForm
 
 
 @auth.get("/login")
 def login_get() -> str:
     """View function for "/login" route."""
-    return render_template("auth/login.html.jinja")
+    form = LoginForm()
+    return render_template("auth/login.html.jinja", form=form)
