59d3e71c0dd1e3223571656bae27cc5faa6e530f
M <m@mathsistor.com>
2022-10-14 00:17:07 -0500

    Fix db migration issue (#58)
    * Pass metadata object to SQLAlchemy
    
    * Fix migration generation
    
    * Order columns

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,24 @@ from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 from config import ENABLED_FOR_DEV, Config
 
 # pylint: disable=fixme,import-outside-toplevel
 
 bootstrap = Bootstrap5()
-db = SQLAlchemy()
+db = SQLAlchemy(
+    metadata=MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+)
 login_manager = LoginManager()
 login_manager.login_view = "auth.login_get"
 migrate = Migrate()
