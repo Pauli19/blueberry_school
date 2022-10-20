83ac3b5f2759a505abd63a894c2f2543da2aa00c
Dora <110313250+Pauli19@users.noreply.github.com>
2022-10-19 22:17:43 -0500

    Implement student create page (#71)
    * Implement-student-form
    
    * Update logic

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,18 +1,25 @@
 """This module contains forms for `admin` blueprint."""
 
 from flask_wtf import FlaskForm
-from wtforms import EmailField, SelectField, StringField, SubmitField, TelField
-from wtforms.validators import Email, InputRequired, Length
+from sqlalchemy import select
+from wtforms import (
+    DateField,
+    EmailField,
+    SelectField,
+    StringField,
+    SubmitField,
+    TelField,
+)
+from wtforms.validators import Email, InputRequired
 
-from ..models import Sex
+from .. import db
+from ..models import Class, Representative, Sex
 
 
 class RepresentativeForm(FlaskForm):
     """This class represents a form to create a representative."""
 
-    identity_document = StringField(
-        "Identity Document", validators=[InputRequired()]
-    )
+    identity_document = StringField("Identity Document", validators=[InputRequired()])
     first_name = StringField("First Name", validators=[InputRequired()])
     second_name = StringField("Second Name")
     first_surname = StringField("First Surname", validators=[InputRequired()])
@@ -28,3 +35,59 @@ class RepresentativeForm(FlaskForm):
     email = EmailField("Email", validators=[Email()])
     phone_number = TelField("Phone Number", validators=[InputRequired()])
     submit = SubmitField("Create")
+
+
+class StudentForm(FlaskForm):
+    """This class represents a form to create a student."""
+
+    identity_document = StringField("Identity Document", validators=[InputRequired()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    second_name = StringField("Second Name")
+    first_surname = StringField("First Surname", validators=[InputRequired()])
+    second_surname = StringField("Second Surname")
+    sex = SelectField(
+        "Sex",
+        choices=[
+            ("", "---"),
+            (Sex.FEMALE.name, Sex.FEMALE.value),
+            (Sex.MALE.name, Sex.MALE.value),
+        ],
+    )
+    birth_date = DateField("Birth Date", validators=[InputRequired()])
+    email = EmailField("Email", validators=[Email()])
+    phone_number = TelField("Phone Number", validators=[InputRequired()])
+    representative = SelectField("Representative")
+    class_ = SelectField("Class")
+    submit = SubmitField("Create")
+
+    def __init__(self) -> None:
+        super().__init__()
+        representative_choices = [("", "No representative")]
+        representative_choices.extend(
+            [
+                (representative.id, str(representative))
+                for representative in (
+                    db.session.execute(
+                        select(Representative).order_by(
+                            Representative.created_at.desc()
+                        )
+                    )
+                    .scalars()
+                    .all()
+                )
+            ]
+        )
+        self.representative.choices = representative_choices
+
+        class_choices = [("", "No class")]
+        class_choices.extend(
+            [
+                (class_.id, str(class_))
+                for class_ in (
+                    db.session.execute(select(Class).order_by(Class.created_at.desc()))
+                    .scalars()
+                    .all()
+                )
+            ]
+        )
+        self.class_.choices = class_choices
