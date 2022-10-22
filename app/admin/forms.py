1cce55b6b8445e817f490f42c7f617a64ae1eb30
Dora <110313250+Pauli19@users.noreply.github.com>
2022-10-22 11:13:56 -0500

    Update app/admin/forms.py
    Co-authored-by: M <m@mathsistor.com>

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -46,7 +46,7 @@ class DeleteButtonWidget:  # pylint: disable=too-few-public-methods
 
 
 class DeleteForm(FlaskForm):
-    """This form represents a form to delete instances."""
+    """This class represents a form to delete instances."""
 
     delete = SubmitField(widget=DeleteButtonWidget())
 
